691aeddfb14ca1e02fc0ebfb72cf95111b3babfb
Divyesh <pateldivyesh1323@gmail.com>
2023-03-30 22:29:57 +0530

    Testing

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,9 +11,11 @@ connectToMongo();
 app.use(express.json());
 
 // Available Routes
+app.use('/',(req,res)=>{
+    console.log("Hello, I am INotebook Backend");
+})
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
-
 app.listen(port, () => {
     console.log(`inotebook app listening on port ${port}`)
 })
